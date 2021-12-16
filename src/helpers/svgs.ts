1bec1413c169632f5c14b8d469054ca143bc4011
Priscila Lopez <priscila.lopez18@itca.edu.sv>
2021-12-15 19:10:35 -0600

    Add svg support

---
+++ src/helpers/svgs.ts
@@ -0,0 +1,21 @@
+import times from '../../assets/svg/times.svg';
+import archive from '../../assets/svg/archive.svg';
+import search from '../../assets/svg/search.svg';
+import add_customer from '../../assets/svg/add-customer.svg';
+import plus from '../../assets/svg/plus.svg';
+import home from '../../assets/svg/home.svg';
+import transactions from '../../assets/svg/transactions.svg';
+import analytics from '../../assets/svg/analytics.svg';
+import payments from '../../assets/svg/payments.svg';
+
+export const Svgs = {
+  times,
+  archive,
+  search,
+  add_customer,
+  plus,
+  home,
+  transactions,
+  analytics,
+  payments,
+};
