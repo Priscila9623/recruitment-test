955f41a03c2b0ceb78ccbcdd7664b9b2a8bf4413
Priscila Lopez <priscila.lopez18@itca.edu.sv>
2021-12-15 21:51:19 -0600

    Add font families

---
+++ src/styles/typography.ts
@@ -0,0 +1,9 @@
+export const FontFiles = {
+  LatoBlack: 'Lato-Black',
+  LatoBold: 'Lato-Bold',
+  LatoLight: 'Lato-Light',
+  LatoRegular: 'Lato-Regular',
+  RobotoBold: 'Roboto-Bold',
+  RobotoMedium: 'Roboto-Medium',
+  RobotoRegular: 'Roboto-Regular',
+};
